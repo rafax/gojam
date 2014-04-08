a4078e94de2f898499566c1d06bd2a6cf462fc59
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2014-04-08 21:47:55 +0200

    Hammer, but not precise enough

---
+++ hammer/hammer.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	g float64 = 9.8
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	var testCases int
+	scanner.Scan()
+	fmt.Fscanf(strings.NewReader(scanner.Text()), "%d", &testCases)
+	for i := 0; i < testCases; i++ {
+		fmt.Printf("Case #%v: %3.7f\n", i+1, solve(parseTestCase(scanner)))
+	}
+}
+
+func solve(v, d int) float64 {
+	sin := float64(d) * g / math.Pow(float64(v), 2.0)
+	return float64(28.647889756541160438399077407052585166202736233282) * math.Asin(sin)
+}
+
+func parseTestCase(scanner *bufio.Scanner) (int, int) {
+	scanner.Scan()
+	pair := strings.Split(scanner.Text(), " ")
+
+	v, _ := strconv.Atoi(pair[0])
+	d, _ := strconv.Atoi(pair[1])
+	return v, d
+}
