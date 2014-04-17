f0427ceb3ab0ae0876471a1478e0e9d21f2a894e
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2014-04-17 18:48:52 +0200

    Codejam man

--- cookie/cookie.go
+++ cookie/cookie.go
@@ -6,10 +6,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 )
 
-var cookiesPerMinute = 2.0
+var errors []string = []string{"Bad magician!", "Volunteer cheated!"}
 
 func main() {
 	var _ = time.Sleep
@@ -18,55 +17,36 @@ func main() {
 	scanner.Scan()
 	fmt.Fscanf(strings.NewReader(scanner.Text()), "%d", &testCases)
 	for i := 0; i < testCases; i++ {
-		fmt.Printf("Case #%v: %.7f\n", i+1, solve(parseTestCase(scanner)))
-	}
-}
-
-func solve(f, p, g float64) float64 {
-	cpm := cookiesPerMinute
-	t := 0.0
-	c := 0.0
-	for c < g {
-		// time.Sleep(100 * time.Millisecond)
-		no_f_t := (g - c) / cpm
-		next_f_t := (f - c) / cpm
-		//fmt.Printf("\tAt %v we have %v with cpm %v, %v until goal %v until next factory\n", t, c, cpm, no_f_t, next_f_t)
-		if no_f_t < 0.000001 {
-			return t
-		}
-		if no_f_t < next_f_t {
-			t += no_f_t
-			c += cpm * no_f_t
+		res := solve(parseTestCase(scanner))
+		if res < 0 {
+			fmt.Printf("Case #%v: %v\n", i+1, errors[-res])
 		} else {
-			t += next_f_t
-			c += cpm * next_f_t
-		}
-		if next_f_t == 0 {
-			t_with_f := (g - c + f) / (cpm + p)
-			t_without_f := (g - c) / (cpm)
-
-			// fmt.Printf("\t\tWithout f %v, with f %v", t_without_f, t_with_f)
-			if t_with_f < t_without_f {
-				// fmt.Print(" Buy\n")
-				c -= f
-				cpm += p
-			} else {
-				// fmt.Print(" Not buy\n")
-				t += t_without_f
-				c += cpm * t_without_f
-			}
+			fmt.Printf("Case #%v: %v\n", i+1, res)
 		}
 	}
-	return t
 }
 
-func parseTestCase(scanner *bufio.Scanner) (float64, float64, float64) {
+func solve(a, b []string) int {
+	fmt.Println(a, b)
+}
+
+func parseTestCase(scanner *bufio.Scanner) ([]string, []string) {
+	linea := getSelectedLine(scanner)
+	lineb := getSelectedLine(scanner)
+	return linea, lineb
+}
+
+func getSelectedLine(scanner) []string {
 	scanner.Scan()
-	vars := strings.Split(scanner.Text(), " ")
-
-	f, _ := strconv.ParseFloat(vars[0], 64)
-	p, _ := strconv.ParseFloat(vars[1], 64)
-	g, _ := strconv.ParseFloat(vars[2], 64)
-
-	return f, p, g
+	lineNo := strconv.Atoi(scanner.Text())
+	for i := 0; i < lineNo; i++ {
+		scanner.Scan()
+	}
+	scanner.Scan()
+	for _, symbol := range scanner.Text() {
+		linea = append(linea, string(symbol))
+	}
+	for i := 0; i < 4-lineNo; i++ {
+		scanner.Scan()
+	}
 }
